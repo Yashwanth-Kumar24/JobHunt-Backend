e0e1fdf16e369448c2b04ec1c8f0a5fa3fbbc743
Yashwanth-Kumar24 <yashwanth.jannaikode@gmail.com>
2026-01-02 21:30:04 -0500

    Added Goldmansachs

--- runner.py
+++ runner.py
@@ -9,6 +9,7 @@ from scrapers.uwash import scrape as scrape_uwash
 from scrapers.doordash import scrape as scrape_doordash
 from scrapers.ssc import scrape as scrape_ssc
 from scrapers.synechron import scrape as scrape_synechron
+from scrapers.goldmansachs import scrape as scrape_goldmansachs
 from db_writer import save_jobs
 
 DB_URL = os.environ.get("SUPABASE_DB_URL")
@@ -26,6 +27,7 @@ jobs.extend(scrape_uwash(max_pages=5))
 jobs.extend(scrape_doordash(max_pages=1))
 jobs.extend(scrape_ssc(max_pages=3))
 jobs.extend(scrape_synechron(max_pages=2))
+jobs.extend(scrape_goldmansachs(max_pages=5))
 print("Total Jobs found: ",len(jobs))
 inserted = save_jobs(jobs, DB_URL)
 print("Rows affected: ",inserted)
