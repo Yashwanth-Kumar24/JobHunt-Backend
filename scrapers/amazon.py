f1f08f14e20ed60783fa7236b2d9b70c35d5dc83
Yashwanth-Kumar24 <yashwanth.jannaikode@gmail.com>
2026-01-06 15:57:58 -0500

    Added reject titles to salesforce,fidelity and amazon

--- scrapers/amazon.py
+++ scrapers/amazon.py
@@ -9,7 +9,7 @@ AMAZON_SEARCH_URL = "https://www.amazon.jobs/en/search.json"
 # Reject only senior / leadership roles
 REJECT_TITLE = re.compile(
     r"\b("
-    r"senior|sr\.?|principal|lead|manager|architect|staff|SDM"
+    r"senior|sr\.?|principal|lead|manager|architect|staff|sdm|director"
     r")\b",
     re.IGNORECASE,
 )
